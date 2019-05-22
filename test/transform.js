ad3dd11e0b609a016c584e506fd7a37d6d39f5ef
AlfieriChou <alfierichou@gmail.com>
2019-05-22 14:15:42 +0800

    add transform test

---
+++ test/transform.js
@@ -0,0 +1,45 @@
+const s2json = require('../')
+const Sequelize = require('sequelize')
+const chai = require('chai')
+const expect = chai.expect
+
+const sequelize = new Sequelize('test', 'root', '', {
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  pool: {
+    max: 10,
+    min: 0,
+    idle: 10000
+  },
+  logging: false
+})
+
+describe('test transform!!!', () => {
+  it('transform model!!', done => {
+    const Project = sequelize.define('Project', {
+      title: { type: Sequelize.STRING, comment: 'title' },
+      number: { type: Sequelize.DOUBLE, comment: 'test double' },
+      uuid: { type: Sequelize.UUID, comment: 'test uuid' },
+      description: { type: Sequelize.TEXT, comment: 'description' }
+    })
+    const transformModel = s2json.transform(Project)
+    expect(transformModel).to.eql(
+      {
+        type: 'object',
+        properties:
+        {
+          id: { type: 'integer', format: 'int32', description: undefined },
+          title: { type: 'string', maxLength: 255, description: 'title' },
+          number: { type: 'number', format: 'double', description: 'test double' },
+          uuid: { type: 'string', format: 'uuid', description: 'test uuid' },
+          description: { type: 'string', description: 'description' },
+          createdAt:
+            { type: 'string', format: 'date-time', description: undefined },
+          updatedAt:
+            { type: 'string', format: 'date-time', description: undefined }
+        }
+      }
+    )
+    done()
+  })
+})
