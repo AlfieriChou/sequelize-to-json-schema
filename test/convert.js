6f4be7a1e2c358b2787d960bf5c1c1d3513d83fc
AlfieriChou <alfierichou@gmail.com>
2019-05-22 12:36:19 +0800

    boolean integer convert test

--- test/convert.js
+++ test/convert.js
@@ -3,39 +3,14 @@ const Sequelize = require('sequelize')
 const chai = require('chai')
 const expect = chai.expect
 
-const convertStr = {
-  test: {
-    type: Sequelize.STRING,
-    comment: 'test field'
-  }
-}
-
-const convertObj = {
-  test: {
-    type: Sequelize.JSON(),
-    keys: {
-      test: {
-        type: Sequelize.STRING,
-        comment: 'test field'
-      }
-    }
-  }
-}
-
-const convertArr = {
-  test: {
-    type: Sequelize.ARRAY(),
-    items: {
-      test: {
-        type: Sequelize.STRING,
-        comment: 'test field'
-      }
-    }
-  }
-}
-
 describe('test convert!!!', () => {
   it('convert string!!', (done) => {
+    const convertStr = {
+      test: {
+        type: Sequelize.STRING,
+        comment: 'test field'
+      }
+    }
     const convertJson = s2json.convert(convertStr)
     expect(convertJson).to.eql(
       {
@@ -48,7 +23,56 @@ describe('test convert!!!', () => {
     done()
   })
 
+  it('convert boolean!!', (done) => {
+    const convertStr = {
+      test: {
+        type: Sequelize.BOOLEAN,
+        comment: 'test field'
+      }
+    }
+    const convertJson = s2json.convert(convertStr)
+    expect(convertJson).to.eql(
+      {
+        type: 'object',
+        properties: {
+          test: { type: 'boolean', description: 'test field' }
+        }
+      }
+    )
+    done()
+  })
+
+  it('convert integer!!', (done) => {
+    const convertStr = {
+      test: {
+        type: Sequelize.INTEGER,
+        comment: 'test field'
+      }
+    }
+    const convertJson = s2json.convert(convertStr)
+    expect(convertJson).to.eql(
+      {
+        type: 'object',
+        properties: {
+          test: { type: 'integer', description: 'test field' }
+        }
+      }
+    )
+    done()
+  })
+
   it('convert object!!', (done) => {
+    const convertObj = {
+      test: {
+        type: Sequelize.JSON(),
+        keys: {
+          test: {
+            type: Sequelize.STRING,
+            comment: 'test field'
+          }
+        }
+      }
+    }
     const convertJson = s2json.convert(convertObj)
     expect(convertJson).to.eql(
       {
@@ -67,6 +91,17 @@ describe('test convert!!!', () => {
   })
 
   it('convert array!!', (done) => {
+    const convertArr = {
+      test: {
+        type: Sequelize.ARRAY(),
+        items: {
+          test: {
+            type: Sequelize.STRING,
+            comment: 'test field'
+          }
+        }
+      }
+    }
     const convertJson = s2json.convert(convertArr)
     expect(convertJson).to.eql(
       {
